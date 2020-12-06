1d21ca722db73762994d165dd5b22f1356ddd020
Keshav-Mamba-Yadav <53308959+Keshav-Mamba-Yadav@users.noreply.github.com>
2020-12-06 20:27:41 +0530

    Add files via upload

---
+++ assignment/assingment2.java
@@ -0,0 +1,14 @@
+package assignment;
+import java.io.*; 
+public class assingment2 {
+	  public static void main(String[] args)throws Exception 
+	  { 
+	String sc = "ss,w/d,";
+	String[] in = sc.split("[\\s@&.?$+-]+");
+	System.out.println(in[0]);
+	  } 
+	    }
+	 
+	
+
+
